c5416a2fd46747a7d9a03cf4f8178b8252532bd1
Riki Syahputra <kosiriki@gmail.com>
2020-07-10 18:48:15 +0700

    add home screen

--- App/index.js
+++ App/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { SafeAreaView, View, StyleSheet, Text } from 'react-native';
 
 
-import { Login, Home } from './screens';
+import { Login, Home, Detail } from './screens';
 
 export default () => {
         return (
             <SafeAreaView style={styles.container}>
                 <View style={{ marginTop: 30 }}>
-                    <Home />
+                    <Detail />
                 </View>
             </SafeAreaView>
         )
