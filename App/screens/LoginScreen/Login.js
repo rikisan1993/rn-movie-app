b57997742811231da9c4c462c9a983d3ade68df6
Riki Syahputra <kosiriki@gmail.com>
2020-07-10 09:36:24 +0700

    add expo-fonts

--- App/screens/LoginScreen/Login.js
+++ App/screens/LoginScreen/Login.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
+
+import Icon from '@expo/vector-icons/FontAwesome5';
 
 import { ScreenContainer } from '../../components';
 
 export const Login = () => {
     return (
         <ScreenContainer>
-            <Text>Login Page</Text>
+            <View style={styles.logo}>
+                <Text>The</Text>
+                <Text>Movie App</Text>
+            </View>
+            <View style={styles.loginForm}>
+                <View style={styles.inputWrapper}>
+                    <Icon name='envelope' size={40} />
+                    <TextInput />
+                </View>
+            </View>
         </ScreenContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    logo: {
+
+    },
+    loginForm: {
+
+    }
+})
