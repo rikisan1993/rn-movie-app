cd706762fc7384b75878225bc43738bff0de57c4
Riki Syahputra <kosiriki@gmail.com>
2020-07-11 18:03:44 +0700

    minor styling update

--- App/screens/LoginScreen/Login.js
+++ App/screens/LoginScreen/Login.js
@@ -1,66 +1,35 @@
 import React from 'react';
 import { View, StyleSheet, Text, TextInput, TouchableOpacity } from 'react-native';
 import Icon from '@expo/vector-icons/FontAwesome5';
-import * as Font from 'expo-font';
 import { AuthContext } from '../../auth-context';
 
-const customFonts = {
-    'Lora-Bold': require('../../../assets/fonts/Lora-Bold.ttf'),
-    'Lora-Regular': require('../../../assets/fonts/Lora-Regular.ttf'),
-    'Source-Sans-Pro-Regular': require('../../../assets/fonts/SourceSansPro-Regular.ttf'),
-    'Source-Sans-Pro-Bold': require('../../../assets/fonts/SourceSansPro-Bold.ttf')
-}
-
 export const Login = () => {
-    const [fontsLoaded, setFontsLoaded] = React.useState(false);
-
-    const loadFont = async () => {
-        await Font.loadAsync(customFonts)
-        setFontsLoaded(true);
-    }
-
-    React.useEffect(() => {
-        let cancelled = false;
-        if(!cancelled) loadFont()
-
-        return () => {
-            cancelled = true;
-        }
-    })
 
     const { signin } = React.useContext(AuthContext);
 
-    if(fontsLoaded) {
-        return (
-            <View style={styles.container}>
-                <View style={styles.logo}>
-                    <Text style={styles.logoPrefix}>The</Text>
-                    <Text style={styles.logoSuffix}>Movie App</Text>
+    return (
+        <View style={styles.container}>
+            <View style={styles.logo}>
+                <Text style={styles.logoPrefix}>The</Text>
+                <Text style={styles.logoSuffix}>Movie App</Text>
+            </View>
+            <View style={styles.loginForm}>
+                <View style={styles.inputWrapper}>
+                    <Icon style={styles.loginIcon} name='envelope' size={20} />
+                    <TextInput style={styles.loginInput} placeholder='e-mail address' />
                 </View>
-                <View style={styles.loginForm}>
-                    <View style={styles.inputWrapper}>
-                        <Icon style={styles.loginIcon} name='envelope' size={20} />
-                        <TextInput style={styles.loginInput} placeholder='e-mail address' />
-                    </View>
-                    
-                    <View style={styles.inputWrapper}>
-                        <Icon style={styles.loginIcon} name='lock' size={20} />
-                        <TextInput style={styles.loginInput} placeholder='password' secureTextEntry={true} />
-                    </View>
+                
+                <View style={styles.inputWrapper}>
+                    <Icon style={styles.loginIcon} name='lock' size={20} />
+                    <TextInput style={styles.loginInput} placeholder='password' secureTextEntry={true} />
+                </View>
 
-                    <TouchableOpacity style={styles.loginButton} onPress={() => signin({token: 'test'})}>
-                        <Text style={styles.loginButtonText}>login</Text>
-                    </TouchableOpacity>
-                </View>
+                <TouchableOpacity style={styles.loginButton} onPress={() => signin({token: 'test'})}>
+                    <Text style={styles.loginButtonText}>login</Text>
+                </TouchableOpacity>
             </View>
-        )
-    } else {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Loading ...</Text>
-            </View>
-        )
-    }
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -68,27 +37,24 @@ const styles = StyleSheet.create({
         height: '100%'
     },
     logo: {
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
         alignSelf: 'center',
         position: 'absolute',
         top: 100,
-        flexDirection: 'row',
         flexWrap: 'wrap',
-        width: '50%'
+        width: 'auto'
     },
     logoPrefix: {
         textAlign: 'left',
-        width: '100%',
         fontFamily: 'Lora-Regular',
         fontSize: 16
     },
     logoSuffix: {
         textAlign: 'left',
-        width: '100%',
-        fontSize: 36,
+        fontSize: 42,
         fontFamily: 'Lora-Bold',
-        marginTop: -25
+        marginTop: -15
     },
     loginForm: {
         position: 'absolute',
@@ -98,7 +64,7 @@ const styles = StyleSheet.create({
     },
     inputWrapper: {
         flexDirection: 'row',
-        backgroundColor: '#F0F0F0',
+        backgroundColor: '#e3e3e3',
         paddingVertical: 8,
         paddingHorizontal: 16,
         alignItems: 'center',
